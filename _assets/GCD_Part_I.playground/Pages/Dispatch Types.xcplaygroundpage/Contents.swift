d9797bd2ad45e64bee37e82f0ec908ef98614d89
Arseniy Kuznetsov <k.arseniy@gmail.com>
2025-09-20 11:04:30 +0100

    update links

--- _assets/GCD_Part_I.playground/Pages/Dispatch Types.xcplaygroundpage/Contents.swift
+++ _assets/GCD_Part_I.playground/Pages/Dispatch Types.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 /*:
  ## Practical GCD, Part I: Introduction to queues
- > This is the playground with sample code for the following [blog](http://www.akpdev.com/articles/2016/04/15/GCD-I.html)
+ > This is the playground with sample code for the following [blog](https://akpw.github.io/articles/2016/04/15/GCD-I.html)
  ****
  [First Page](QOS%20Basics) | [Previous](@previous) | [Next](@next)
  */
